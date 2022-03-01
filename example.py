23cb4fa5d42923a15fff9e4eb0679d965e26e4f3
Windy Feng <1745500559@qq.com>
2022-02-28 19:07:42 -0800

    V2 Release

---
+++ example.py
@@ -0,0 +1,17 @@
+# Implement the quadratic function below
+def quadratic(a, b, c, x):
+    # YOUR CODE STARTS HERE
+    return
+    # YOUR CODE ENDS HERE
+
+# test 1
+a = 2
+b = -5
+c = 10
+x = 2
+y = quadratic(a, b, c, x)
+expected_y = 8
+if (expected_y != y):
+    print("*** Test Failed! ***\nThe expeceted output is %s" %(8))
+else:
+    print("*** Test Passed! ***")
